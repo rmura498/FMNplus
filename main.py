8d1976da91b87c80d26a7288320bc49c388571b2
Raffaele Mura <rff.mura@gmail.com>
2023-11-21 12:13:55 +0100

    removing misclassified samples

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import math
 import os
 import pickle
 from Attacks.fmn import FMN
+import numpy as np
 from Utils.load_model import load_dataset, load_model
 from robustbench.utils import clean_accuracy
 from Utils.plots import plot_distance
 from Utils.fmn_strategies import fmn_strategies
+from Utils.comparing_strategies import evaluate_strategies
 
 """
 FMN parametric strategy 
@@ -19,20 +21,45 @@ FMN parametric strategy
 """
 
 
-def run_experiments(steps=50, batch_size=10, batch_number=1):
+def run_experiments(steps=30, batch_size=10, batch_number=1):
+    # model definition
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = load_model('Gowal2021Improving_R18_ddpm_100m', 'cifar10')
     dataset = load_dataset('cifar10')
     model.eval()
     model.to(device)
 
+    # retrieving FMN set of configurations
     fmn_dict = fmn_strategies()
 
+    # dataset definition
     dataset_frac = list(range(math.floor(len(dataset) * 0.5) + 1, len(dataset)))
     dataset_frac = torch.utils.data.Subset(dataset, dataset_frac)
-    dl_test = torch.utils.data.DataLoader(dataset_frac, batch_size=batch_size, shuffle=False)
-    dl_test_iter = iter(dl_test)
-    samples, labels = next(dl_test_iter)
+    dl_test = torch.utils.data.DataLoader(dataset_frac, batch_size=1, shuffle=False)
+
+    samples = torch.empty([1, 3, 32, 32])
+    labels = torch.empty([1], dtype=torch.int64)
+    i = 0
+
+
+    for sample, label in dl_test:
+        y_pred = model(sample.reshape(1, 3, 32, 32))
+        y_pred = torch.argmax(y_pred)
+
+        if y_pred == label:
+            samples = torch.cat([samples, sample])
+            labels = torch.cat([labels, label])
+            i += 1
+        if i >= batch_size:
+            break
+    print(samples)
+    samples = samples[1:]
+    labels = labels[1:]
+
+    print(labels.shape)
+    clean_acc = clean_accuracy(model, samples, labels)
+    print("clean accuracy", clean_acc)
+    print(labels)
 
     for i in range(len(fmn_dict)):
         print(i)
@@ -41,8 +68,7 @@ def run_experiments(steps=50, batch_size=10, batch_number=1):
                      scheduler=fmn_dict[str(i)]['scheduler'], gradient_strategy=fmn_dict[str(i)]['gradient_strategy'],
                      initialization_strategy=fmn_dict[str(i)]['initialization_strategy'], device=device)
         adv_x, best_distance = attack.forward(samples, labels)
-        clean_acc = clean_accuracy(model, samples, labels, device=device)
-        print("Clean Accuracy", clean_acc)
+
         robust_acc = clean_accuracy(model, adv_x, labels, device=device)
         print(f"Robust Accuracy:{fmn_dict[str(i)]}", robust_acc)
         attack_data = attack.attack_data
@@ -58,6 +84,8 @@ def run_experiments(steps=50, batch_size=10, batch_number=1):
         with open(filename, 'wb') as file:
             pickle.dump(attack_data, file)
 
+    evaluate_strategies(batch_size)
+
 
 run_experiments()
-#plot_distance()
+# plot_distance()
