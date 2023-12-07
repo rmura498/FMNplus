bb8b6c817a03317e50e10a6e6c69f6580b261a4c
Luca Scionis <lucascionis@tiscali.it>
2023-12-07 11:54:06 +0100

    Testing FMN Vec with FMN Base

--- test_v_scheduler_2.py
+++ test_v_scheduler_2.py
@@ -1,4 +1,4 @@
-import pickle
+import os, pickle
 from datetime import datetime
 
 import torch
@@ -14,25 +14,33 @@ from Utils.load_model import load_data
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-if __name__ == '__main__':
-    batch_size = 50
-    num_batches = 1
-    steps = 20
-    loss = 'CE'
-    optimizer = 'Adam'
-    norm = inf # same as float('inf')
-    alpha_init = 1
-    alpha_final = None
+
+def main(
+        batch_size = 10,
+        num_batches = 1,
+        steps = 30,
+        epsilon = 8/244, # None for dynamic one
+        loss='CE',
+        optimizer = 'Adam',
+        scheduler = 'RLROP',
+        norm = inf,  # same as float('inf')
+        alpha_init = 1,
+        alpha_final = None,
+        verbose = True,
+        model_id = 8,
+        plot = False,
+        shuffle=True
+):
 
     # loading model and dataset
-    model, dataset, model_name, dataset_name = load_data(model_id=8)
+    model, dataset, model_name, dataset_name = load_data(model_id=model_id)
     model.eval().to(device)
 
     # Dataloader
     dataloader = DataLoader(
         dataset=dataset,
         batch_size=batch_size,
-        shuffle=True
+        shuffle=shuffle
     )
 
     # Instantiating FMN vec
@@ -41,11 +49,12 @@ if __name__ == '__main__':
         steps=steps,
         loss=loss,
         device=device,
-        epsilon=8/255,
+        epsilon=epsilon,
         optimizer=optimizer,
         norm=norm,
         alpha_init=alpha_init,
-        alpha_final=alpha_final
+        alpha_final=alpha_final,
+        verbose=verbose
     )
 
     fmn_base = FMNBase(
@@ -53,15 +62,14 @@ if __name__ == '__main__':
         steps=steps,
         loss=loss,
         device=device,
-        epsilon=8 / 255,
+        epsilon=epsilon,
         optimizer=optimizer,
-        scheduler='RLROP',
+        scheduler=scheduler,
         norm=norm,
         alpha_init=alpha_init,
-        alpha_final=alpha_final
+        alpha_final=alpha_final,
+        verbose=verbose
     )
-
-    loss_fmn_base = []
 
     for i, (samples, labels) in enumerate(dataloader):
         # clean misclassified
@@ -71,35 +79,9 @@ if __name__ == '__main__':
         samples = samples[correctly_classified_samples]
         labels = labels[correctly_classified_samples]
 
+        print("\n\nRunning FMN vec\n")
         _, _ = fmn_vec.forward(samples, labels)
-
         print("\n\nRunning FMN base\n")
-
-        # running multiple FMNBase instances
-        # re-instantiating FMN base
-        '''
-        for j, (sample, label) in enumerate(zip(samples, labels)):
-            sample.unsqueeze_(0)
-            label.unsqueeze_(0)
-
-            print(f"Sample, label shapes at [{i}]:\n{sample.shape}\n{label}")
-            fmn_base = FMNBase(
-                model,
-                steps=steps,
-                loss=loss,
-                device=device,
-                epsilon=8 / 255,
-                optimizer=optimizer,
-                scheduler='RLROP',
-                norm=norm,
-                alpha_init=alpha_init,
-                alpha_final=alpha_final
-            )
-            _, _ = fmn_base.forward(sample, label)
-            loss_fmn_base.append(fmn_base.attack_data['loss'])
-            print(f"Cur loss len: {len(fmn_base.attack_data['loss'])}")
-        '''
-
         _, _ = fmn_base.forward(samples, labels)
 
         if i+1 == num_batches: break
@@ -111,29 +93,37 @@ if __name__ == '__main__':
         'loss_fmn_vec': fmn_vec.attack_data['loss']
     }
 
+    print(f"\nFMN Base loss:\n{loss_fmn_base}")
+    print(f"FMN Vec loss:\n{fmn_vec.attack_data['loss']}")
+
+    epsilon_name = "8-255" if epsilon == 8/255 else "None"
     current_date = datetime.now()
     formatted_date = current_date.strftime("%d%m%y")
-    filename = (f"SchedulerVecExps/FMN_vecVsBase_lossData_{formatted_date}"
-                f"_{optimizer}_steps{steps}_batch{batch_size}.pkl")
+
+    exp_path = os.path.join("SchedulerVecExps", formatted_date)
+    if not os.path.exists(exp_path):
+        os.makedirs(exp_path)
+    filename = os.path.join(exp_path, f"FMN_vecVsBase_lossData_{optimizer}_steps{steps}"
+                                      f"_batch{batch_size}_eps_{epsilon_name}.pkl")
 
     with open(filename, 'wb') as file:
         pickle.dump(loss_data, file)
 
-    # plot losses
-    fig, ax = plt.subplots(figsize=(4, 4))
+    if plot:
+        # plot losses
+        fig, ax = plt.subplots(figsize=(4, 4))
 
-    steps_x = np.arange(0, steps)
-    ax.plot(steps_x, loss_fmn_base, label='FMN Base loss')
-    ax.plot(steps_x, fmn_vec.attack_data['loss'], label='FMN Vec loss', linestyle='dotted')
+        steps_x = np.arange(0, steps)
+        ax.plot(steps_x, loss_fmn_base, label='FMN Base loss')
+        ax.plot(steps_x, fmn_vec.attack_data['loss'], label='FMN Vec loss', linestyle='dotted')
 
-    ax.grid()
-    ax.legend()
+        ax.grid()
+        ax.legend()
 
-    # Get current date
-    current_date = datetime.now()
-    formatted_date = current_date.strftime("%d%m%y")
-    fig.savefig(f"SchedulerVecExps/FMN_vecVsBase_lossData_{formatted_date}"
-                f"_{optimizer}_steps{steps}_batch{batch_size}.pdf")
-
+        fig_path = os.path.join(exp_path, f"SchedulerVecExps/FMN_vecVsBase_lossData"
+                                          f"_{optimizer}_steps{steps}_batch{batch_size}_eps_{epsilon_name}.pdf")
+        fig.savefig(fig_path)
 
 
+if __name__ == '__main__':
+    main(batch_size=5, steps=50)
