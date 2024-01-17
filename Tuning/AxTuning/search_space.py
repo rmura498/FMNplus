d20645e27504017636d613b37144f3d68f5cf03c
Luca Scionis <lucascionis@tiscali.it>
2024-01-17 15:48:18 +0100

    Created tuning_fmn_with_ax.py, search_space.py
    - tuning_fmn_with_ax.py: script for fmn hyperparams optimization using Ax framework
    - Tuning/AxTunig/search_space.py: defined the params for the Ax tuning search space

---
+++ Tuning/AxTuning/search_space.py
@@ -0,0 +1,45 @@
+"""
+Hyperparams to be optimized using Ax
+"""
+
+OptimizerParams = {
+    'SGD': {
+        'lr': {"name": "lr", "type": "range", "bounds": [(8/255)*2, 10], "value_type": "float", "log_scale": True},
+        'momentum': {"name": "momentum", "type": "range", "bounds": [0.0, 0.9], "value_type": "float"},
+        'weight_decay': {"name": "weight_decay", "type": "range", "bounds": [0.01, 1.0], "value_type": "float"},
+        'dampening': {"name": "dampening", "type": "range", "bounds": [0.0, 0.2], "value_type": "float"}
+    },
+    'Adam':
+    {
+        'lr': {"name": "lr", "type": "range", "bounds": [(8/255)*2, 10], "value_type": "float", "log_scale": True},
+        'weight_decay': {"name": "weight_decay", "type": "range", "bounds": [0.01, 1.0], "value_type": "float"},
+        'eps': {"name": "eps", "type": "fixed", "value": 1e-8, "value_type": "float"},
+        'beta1': {"name": "beta1", "type": "range", "bounds": [0.0, 0.999], "value_type": "float"},
+        'beta2': {"name": "beta2", "type": "range", "bounds": [0.0, 0.999], "value_type": "float"}
+    },
+    'Adamax':
+    {
+        'lr': {"name": "lr", "type": "range", "bounds": [(8/255)*2, 10], "value_type": "float", "log_scale": True},
+        'weight_decay': {"name": "weight_decay", "type": "range", "bounds": [0.01, 1.0], "value_type": "float"},
+        'eps': {"name": "eps", "type": "fixed", "value": 1e-8, "value_type": "float"},
+        'beta1': {"name": "beta1", "type": "range", "bounds": [0.0, 0.999], "value_type": "float"},
+        'beta2': {"name": "beta2", "type": "range", "bounds": [0.0, 0.999], "value_type": "float"}
+    }
+}
+
+SchedulerParams = {
+    'CALR': {
+        'T_max': lambda steps: {"name": "T_max", "type": "fixed", "value": steps, "value_type": "int"},
+        'eta_min':  {"name": "eta_min", "type": "fixed", "value": 0, "value_type": "int"},
+        'last_epoch': {"name": "last_epoch", "type": "fixed", "value": -1, "value_type": "int"}
+    },
+    'RLROPVec':
+    {
+        'batch_size': lambda bs: {"name": "batch_size", "type": "fixed", "value": bs, "value_type": "int"},
+        'factor': {"name": "factor", "type": "range", "bounds": [0.1, 0.5], "value_type": "float"},
+        'patience': {"name": "patience", "type": "choice", "bounds": [2, 5, 10], "value_type": "int"},
+        'threshold': {"name": "threshold", "type": "fixed", "value": 1e-4, "value_type": "float"},
+        'eps': {"name": "eps", "type": "fixed", "value": 1e-8, "value_type": "float"}
+    }
+}
+
---
+++ Tuning/tuning_fmn_with_ax.py
@@ -0,0 +1,130 @@
+import argparse
+from datetime import datetime
+
+import torch
+from torch.utils.data import DataLoader
+from ax.service.ax_client import AxClient, ObjectiveProperties
+
+from Attacks.fmn_base import FMN
+from Utils.load_model import load_data
+from AxTuning.search_space import OptimizerParams, SchedulerParams
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+parser = argparse.ArgumentParser(description="Tune FMN Hyperparameters with Ax")
+
+parser.add_argument('--model_id', type=int, default=8, help='Robustbench model\'s id')
+parser.add_argument('--batch_size', type=int, default=32, help='Size of a single batch')
+parser.add_argument('--shuffle', type=bool, default=False, help='Shuffle samples of each batch')
+parser.add_argument('--optimizer', type=str, default='Adam', choices=['Adam', 'SGD', 'Adamax'], help='Optimizer for the attack')
+parser.add_argument('--scheduler', type=str, default=None, choices=['RLROPVec', 'CALR', None],  help='Scheduler for the attack')
+parser.add_argument('--steps', type=int, default=20, help='Steps of the attack')
+parser.add_argument('--loss', type=str, default='CE', help='Loss for the attack')
+parser.add_argument('--epsilon', type=float, default=None, help='Variable (None) or fixed epsilon ball size')
+parser.add_argument('--norm', type=float, default=float('inf'), help='Type of norm (e.g. Linf, L0, L1, L2)')
+parser.add_argument('--gradient_update', type=str, default='Sign', choices=['Normalization', 'Projection', 'Sign'], help='Attack\'s gradient update strategy')
+parser.add_argument('--n_trials', type=int, default=1, help='How many hyperparams optimization trials')
+parser.add_argument('--device', type=str, default='cpu', choices=['RLROPVec', 'CALR', None], help='Device to use (cpu, cuda:0, cuda:1)')
+parser.add_argument('--cuda_device', type=int, default=None, help='Specific gpu to use like 0, 1 etc - (optional)')
+
+
+args = parser.parse_args()
+
+model_id = int(args.model_id)
+batch_size = int(args.batch_size)
+shuffle = bool(args.shuffle)
+optimizer = args.optimizer
+scheduler = str(args.scheduler) if args.scheduler else args.scheduler
+steps = int(args.steps)
+loss = args.loss
+epsilon = float(args.epsilon) if args.epsilon else args.epsilon
+norm = float(args.norm)
+gradient_update = args.gradient_update
+n_trials = int(args.n_trials)
+
+
+def attack_evaluate(parametrization):
+    optimizer_config = {k: parametrization[k] for k in set(opt_params)}
+    scheduler_config = None
+    if scheduler is not None:
+        scheduler_config = {k: parametrization[k] for k in set(sch_params)}
+
+    attack = FMN(
+        model=model,
+        steps=steps,
+        loss=loss,
+        device=device,
+        epsilon=epsilon,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        norm=norm,
+        gradient_strategy=gradient_update,
+        optimizer_config=optimizer_config,
+        scheduler_config=scheduler_config
+    )
+
+    _, best_distance, _ = attack.forward(images=images, labels=labels)
+    best_distance = float(best_distance.mean().item())
+    # sr = float(sr)
+
+    return {'distance': (best_distance, 0.0)}
+
+
+# Retrieve optimizer and scheduler params
+opt_params = OptimizerParams[optimizer]
+sch_params = None
+if scheduler is not None:
+    sch_params = SchedulerParams[scheduler]
+    if 'T_max' in sch_params:
+        sch_params['T_max'] = sch_params['T_max'](steps)
+    if 'batch_size' in sch_params:
+        sch_params['batch_size'] = sch_params['batch_size'](batch_size)
+
+# Loading model and dataset
+print("\t[Tuning] Retrieving the model and the dataset...")
+model, dataset, model_name, dataset_name = load_data(model_id=model_id)
+model.eval()
+model = model.to(device)
+
+dataloader = DataLoader(
+    dataset=dataset,
+    batch_size=batch_size,
+    shuffle=shuffle
+)
+images, labels = next(iter(dataloader))
+
+# Create Ax Experiment
+print("\t[Tuning] Creating the Ax client and experiment...")
+ax_client = AxClient()
+
+# Defining the Search Space
+if scheduler is not None:
+    params = list(opt_params.values()) + list(sch_params.values())
+else:
+    params = list(opt_params.values())
+
+# Create an experiment with required arguments: name, parameters, and objective_name.
+experiment_name = f'mid{model_id}_{batch_size}_{steps}_{optimizer}"\
+                    f"_{scheduler}_{loss}_{gradient_update}'
+ax_client.create_experiment(
+    name=experiment_name,
+    parameters=params,
+    objectives={"distance": ObjectiveProperties(minimize=True)}
+)
+
+print("\t[Tuning] Starting the Hyperparameters Optimization...")
+for i in range(n_trials):
+    print("\t[Tuning] Running trial {}")
+    parameters, trial_index = ax_client.get_next_trial()
+    ax_client.complete_trial(trial_index=trial_index, raw_data=attack_evaluate(parameters))
+
+print("\t[Tuning] Finished the Hyperparameters Optimization; printing the trials list and best parameters...")
+print(ax_client.get_trials_data_frame())
+
+best_parameters, values = ax_client.get_best_parameters()
+print("\t[Tuning] Best parameters: ", best_parameters)
+
+print("\t[Tuning] Saving the experiment data...")
+current_date = datetime.now()
+formatted_date = current_date.strftime("%d%m%y%H")
+ax_client.save_to_json_file(filepath=f'{formatted_date}_{experiment_name}.json')
