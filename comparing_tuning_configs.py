30cdffd5de81354b2e1fe324b8ade4cd868413e9
Raffaele Mura <rff.mura@gmail.com>
2024-01-22 18:29:20 +0100

    updated comparing_tuning_configs.py

--- comparing_tuning_configs.py
+++ comparing_tuning_configs.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import numpy as np
 import pandas as pd
 
 from ax.service.ax_client import AxClient
@@ -30,43 +29,24 @@ def tuning_comparison(model_id, PATH):
         ax_client.get_next_trial()
 
         best_params = ax_client.get_pareto_optimal_parameters()
-        best_params = best_params.items()[0]
-        print(best_params)
-        best_params, values = best_params
-        print(best_params, values)
-        exit(0)
+        best_params = list(best_params.items())
+        best_params_dict = best_params[0][1][0]
+        best_results = best_params[0][1][1][0]
 
-        check_mid = configs.get(mid, None)
-        check_conf = None
-
-        if check_mid is not None:
-            check_conf = configs[mid].get(conf_name, None)
-        else:
-            configs[mid] = {}
-
-        if check_conf is None:
-            configs[mid][conf_name] = [sr_data, ]
-        else:
-            configs[mid][conf_name].append(sr_data)
-
-    sr_df = {}
-    for mid in configs:
-        for config in configs[mid]:
-            configs[mid][config] = np.array(configs[mid][config]).mean().round(decimals=2)
-
-    sr_df = pd.DataFrame(configs)
-    sr_df['SR-mean'] = sr_df.mean(axis=1)
-    sr_df = sr_df.sort_values('SR-mean', ascending=False)
-
-    print(sr_df)
-
-    sr_df = sr_df.style.highlight_max(
-        props='cellcolor:[HTML]{b666cc}; color:{white};' 'textit:--rwrap; textbf:--rwrap;').format("{:.2f}")
-
+        configs_dict[conf_name] = {'Best Params':str(best_params_dict),
+                                   'Best Distance':best_results['distance'],
+                                   'SR':best_results['sr']}
+    print(configs_dict)
+    pd.set_option('display.max_colwidth', None)
+    df = pd.DataFrame(configs_dict)
+    print(df)
+    df = df.transpose()
+    df = df.sort_values(by=['SR'], ascending=False)
+    print(df)
     from datetime import datetime
     current_date = datetime.now()
     formatted_date = current_date.strftime("%d%m%y%H")
-    sr_df.to_latex(f'sr_comparison_{formatted_date}.txt')
+    df.to_latex(f'sr_comparison_{formatted_date}.txt')
 
 
 tuning_comparison(model_id=0, PATH='tuning_results/ax_tuning_jan24/mid0')
