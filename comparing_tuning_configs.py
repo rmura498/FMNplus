1ec7b2befa48f26e1ee12fa27a6cb4a1fadc862d
Raffaele Mura <rff.mura@gmail.com>
2024-01-29 09:55:41 +0100

    updated tuning strategy

--- comparing_tuning_configs.py
+++ comparing_tuning_configs.py
@@ -18,7 +18,7 @@ def tuning_comparison(model_id, PATH):
     configs_dict = {}
     json_files = Path(PATH).rglob('*.json')
 
-    for json_file in json_files:
+    for i, json_file in enumerate(json_files):
         date, mid, batch_size, steps, trials, opt, sch, loss = extact_data(json_file.name)
         conf_name = f'{opt}-{sch}-{loss}'
 
@@ -26,27 +26,26 @@ def tuning_comparison(model_id, PATH):
         filepath = PATH + '/' + json_file.name
         ax_client = AxClient().load_from_json_file(filepath=filepath, verbose_logging=False)
 
+        print(conf_name)
         ax_client.get_next_trial()
 
-        best_params = ax_client.get_pareto_optimal_parameters()
-        best_params = list(best_params.items())
-        best_params_dict = best_params[0][1][0]
-        best_results = best_params[0][1][1][0]
+        best_params = ax_client.get_best_parameters()
 
-        configs_dict[conf_name] = {'Best Params':str(best_params_dict),
-                                   'Best Distance':best_results['distance'],
-                                   'SR':best_results['sr']}
-    print(configs_dict)
+        best_params = list(best_params)
+        best_params_dict = best_params[0]
+        best_results = best_params[1][0]
+
+        configs_dict[conf_name] = {'Best Params':best_params_dict,
+                                   'Best Distance':best_results['distance']}
     pd.set_option('display.max_colwidth', None)
     df = pd.DataFrame(configs_dict)
-    print(df)
     df = df.transpose()
-    df = df.sort_values(by=['SR'], ascending=False)
+    df = df.sort_values(by=['Best Distance'], ascending=True)
     print(df)
     from datetime import datetime
     current_date = datetime.now()
     formatted_date = current_date.strftime("%d%m%y%H")
-    df.to_latex(f'sr_comparison_{formatted_date}.txt')
+    df.to_latex(f'sr_comparison-mid{model_id}_{formatted_date}.txt')
 
 
-tuning_comparison(model_id=0, PATH='tuning_results/ax_tuning_jan24/mid0')
+tuning_comparison(model_id=8, PATH='tuning_results/ax_tuning_jan24/mid8_median')
