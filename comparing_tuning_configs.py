a82e4ed4a20c1f82aebcd46df691f000daaca2a2
Luca Scionis <lucascionis@tiscali.it>
2024-02-02 10:17:52 +0100

    Code refactoring

--- comparing_tuning_configs.py
+++ comparing_tuning_configs.py
@@ -1,10 +1,15 @@
+import threading
 from pathlib import Path
-import statistics
-import numpy as np
+
 import pandas as pd
+import numpy as np
 import torch
-from ax.service.ax_client import AxClient
 import matplotlib.pyplot as plt
+
+from ax.service.ax_client import AxClient
+
+columns_to_remove = {'eps', 'T_max', 'eta_min', 'last_epoch', 'batch_size', 'threshold'}
+
 
 def extact_data(filename=None):
     if filename is None:
@@ -18,15 +23,16 @@ def extact_data(filename=None):
 
 def tuning_comparison(model_id, PATH):
     configs_dict = {}
-    json_files = Path(PATH).rglob('*.json')
+    json_files = tuple(Path(PATH).rglob(f'*mid{model_id}*.json'))
+    print(list(json_files))
 
     for i, json_file in enumerate(json_files):
         date, mid, batch_size, steps, trials, opt, sch, loss = extact_data(json_file.name)
         conf_name = f'{opt}-{sch}-{loss}'
 
         # read json file
-        filepath = PATH + '/' + json_file.name
-        ax_client = AxClient().load_from_json_file(filepath=filepath, verbose_logging=False)
+        # filepath = PATH + '/' + json_file.name
+        ax_client = AxClient(verbose_logging=False).load_from_json_file(filepath=json_file)
 
         print(conf_name)
         ax_client.get_next_trial()
@@ -46,64 +52,43 @@ def tuning_comparison(model_id, PATH):
         best_params_dict = best_params[0]
         #print(best_params_dict)
         for k, v in best_params_dict.items():
-
             if k in conv_dict:
                 best_params_dict[k] = conv_dict[k](v)
 
         #print(best_params_dict)
         best_results = best_params[1][0]
 
-        configs_dict[conf_name] = {'Best Params':best_params_dict,
-                                   'Best Distance':best_results['distance']}
+        configs_dict[conf_name] = {**(best_params_dict)}
+        configs_dict[conf_name]['Best Distance'] = best_results['distance']
+
+        if 'beta1' in configs_dict[conf_name]:
+            betas = f"({configs_dict[conf_name]['beta1']},{configs_dict[conf_name]['beta2']})"
+            configs_dict[conf_name]['betas'] = betas
+            del configs_dict[conf_name]['beta1']
+            del configs_dict[conf_name]['beta2']
+
     pd.set_option('display.max_colwidth', None)
     df = pd.DataFrame(configs_dict)
     df = df.transpose()
     df = df.sort_values(by=['Best Distance'], ascending=True)
+
+    # remove NaNs
+    df.fillna(value='-', inplace=True)
+    last_col = len(df.columns)
+    column_to_move = df.pop("Best Distance")
+    df.insert(last_col-1, 'Best Distance', column_to_move)
+
+    for column in df.columns:
+        if column in columns_to_remove:
+            df.drop(column, axis=1, inplace=True)
+
     print(df)
+
     from datetime import datetime
     current_date = datetime.now()
     formatted_date = current_date.strftime("%d%m%y%H")
-    df.to_latex(f'sr_comparison-mid{model_id}_{formatted_date}.txt')
+    df.to_latex(f'tuning_results_mid{model_id}_{formatted_date}.tex')
 
-def tuning_comparison(model_id, PATH):
-    configs_dict = {}
-    json_files = Path(PATH).rglob('*.json')
-
-    for i, json_file in enumerate(json_files):
-        date, mid, batch_size, steps, trials, opt, sch, loss = extact_data(json_file.name)
-        conf_name = f'{opt}-{sch}-{loss}'
-
-        # read json file
-        filepath = PATH + '/' + json_file.name
-        ax_client = AxClient().load_from_json_file(filepath=filepath, verbose_logging=False)
-
-        print(conf_name)
-        ax_client.get_next_trial()
-
-        conv_dict = {
-            'lr': lambda x:f'{x:.4e}',
-            'momentum':lambda x:f'{x:.4f}',
-            'weight_decay':lambda x:f'{x:.3f}',
-            'dampening':lambda x:f'{x:.3f}',
-            'beta1':lambda x:f'{x:.3f}',
-            'beta2':lambda x:f'{x:.3f}',
-            'factor':lambda x:f'{x:.3f}'
-        }
-        print(ax_client.get_best_trial())
-        best_params = ax_client.get_best_parameters()
-        best_params = list(best_params)
-        best_params_dict = best_params[0]
-        #print(best_params_dict)
-        for k, v in best_params_dict.items():
-
-            if k in conv_dict:
-                best_params_dict[k] = conv_dict[k](v)
-
-        #print(best_params_dict)
-        best_results = best_params[1][0]
-
-        configs_dict[conf_name] = {'Best Params':best_params_dict,
-                                   'Best Distance':best_results['distance']}
 
 def trial_comparison(PATH):
 
@@ -148,6 +133,10 @@ def trial_comparison(PATH):
     plt.savefig(f"comparison_trials.pdf", bbox_inches='tight', dpi=320)
 
 
+if __name__ == '__main__':
+    for model_id in range(0, 9):
+        x = threading.Thread(target=tuning_comparison, args=(model_id, './Tuning/AxTuning/exps'))
+        x.start()
+        # tuning_comparison(model_id=model_id, PATH='./Tuning/AxTuning/exps')
 
-trial_comparison('tuning_results/ax_tuning_jan24/mid8_short_exps')
-#tuning_comparison(model_id=8, PATH='tuning_results/ax_tuning_jan24/mid8_short_exps')
+    # tuning_comparison(model_id=3, PATH='./Tuning/AxTuning/exps/mid8')
