e3c710825ee6ea395c03e4e035e879c9584d48e2
Luca Scionis <lucascionis@tiscali.it>
2024-03-05 15:21:43 +0100

    Testing different imagenet transformation

--- Utils/load_model.py
+++ Utils/load_model.py
@@ -22,9 +22,11 @@ def load_dataset(dataset_name='cifar10'):
             transforms.ToTensor()
         ])
     elif dataset_name == 'imagenet':
-        transform = transforms.Compose([transforms.ToTensor(),
-                                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                                        ])
+        transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor()
+        ])
 
     if dataset_name == 'mnist':
         dataset = torchvision.datasets.MNIST('./Models/data',
