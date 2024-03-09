6749ead5ab7d771e832293cadad7b97cbab9510b
Luca Scionis <lucascionis@tiscali.it>
2024-03-09 13:19:34 +0100

    New code for reading imagenet1000 dataset using pytorch dataloader

--- Utils/imagenet_1k.py
+++ Utils/imagenet_1k.py
@@ -1,25 +1,52 @@
-import os
-import pandas as pd
+import csv
+from pathlib import Path
+from collections import defaultdict
 
 import torch
-from torchvision.io import read_image
+from PIL import Image
 
 
 class ImageNet1K(torch.utils.data.Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
+    def __init__(self, dataset_root=None, transform=None, target_transform=None):
+        if dataset_root == None:
+          raise ValueError("Dataset root is None!")
+        self.img_paths, self.img_labels, self.img_target_labels = self._load_imagenet_1000(dataset_root)
         self.transform = transform
         self.target_transform = target_transform
+
+    def _load_imagenet_1000(self, dataset_root):
+      """
+      Dataset downoaded form kaggle
+      https://www.kaggle.com/datasets/google-brain/nips-2017-adversarial-learning-development-set
+      Resized from 299x299 to 224x224
+      Args:
+          dataset_root (str): root folder of dataset
+      Returns:
+          img_paths (list of strs): the paths of images
+          gt_labels (list of ints): the ground truth label of images
+          tgt_labels (list of ints): the target label of images
+      """
+      dataset_root = Path(dataset_root)
+      img_paths = list(sorted(dataset_root.glob('*.png')))
+      gt_dict = defaultdict(int)
+      tgt_dict = defaultdict(int)
+      with open(str(dataset_root) + '/' + "images.csv", newline='') as csvfile:
+          reader = csv.DictReader(csvfile)
+          for row in reader:
+              gt_dict[row['ImageId']] = int(row['TrueLabel'])
+              tgt_dict[row['ImageId']] = int(row['TargetClass'])
+      gt_labels = [gt_dict[key] - 1 for key in sorted(gt_dict)]  # zero indexed
+      tgt_labels = [tgt_dict[key] - 1 for key in sorted(tgt_dict)]  # zero indexed
+      return img_paths, gt_labels, tgt_labels
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        image_name = f"{self.img_labels.iloc[idx, 0]}.png"
-        img_path = os.path.join(self.img_dir, image_name)
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 6]
+        img_path = self.img_paths[idx]
+        # image = read_image(str(img_path), mode=ImageReadMode.RGB)
+        image = Image.open(str(img_path)).convert('RGB')
+        label = torch.Tensor([self.img_labels[idx]])
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
